4cc94f1840e3bd8daaaaa67c01e27266c3bb8fd8
matthew aboyade <aboyadematthew@gmail.com>
2024-08-19 07:49:52 -0700

    add halfway project

--- js/script.js
+++ js/script.js
@@ -15,4 +15,37 @@ window.onload = function () {
         $(".signupin").slideUp()
     })
 
+    $(".def").click(function () {
+        let control = $(".def").index(this);
+        switch (control) {
+            case 0:
+                $(".hide1").fadeIn();
+                $(".hide2").slideUp();
+                $(".hide3").slideUp();
+                break;
+            case 1:
+                $(".hide2").fadeIn();
+                $(".hide1").slideUp();
+                $(".hide3").slideUp();
+                break;
+            default:
+                $(".hide3").fadeIn();
+                $(".hide2").slideUp();
+                $(".hide1").slideUp();
+        }
+    })
+$(".cancel").click(function () {
+    $(".back").slideUp()
+})
+    // $(".fund").click(function () {
+    //     $(".hide1").slideDown()
+
+    // })
+    // $(".ment").click(function () {
+    //     $(".hide2").slideDown()
+    // })
+    // $(".ment2").click(function () {
+    //     $(".hide3").slideDown()
+    // })
+
 }
